790260ce7fe0d689439e7900d296c33d20abee2d
Xavier Rogers <137833857+Astroid45@users.noreply.github.com>
2025-04-30 18:14:26 -0400

    react app env

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from "react";
-export const API_BASE_URL = 'http://localhost:3001';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 // recommendation model
 export const getSampleTracks = async (count = 20) => {
